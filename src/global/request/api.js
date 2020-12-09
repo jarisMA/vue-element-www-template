a8beb50778355893aceaaabce3365e2e2f139d8f
jarisMA <jarisma@foxmail.com>
2020-12-09 16:57:44 +0800

    fix: fix api host

--- src/global/request/api.js
+++ src/global/request/api.js
@@ -1,14 +1,11 @@
-const PREFIX_API = process.env.VUE_APP_HOST;
-// const PREFIX_API = "";
-
 export default {
-  users: `${PREFIX_API}/api/web/users`,
-  logout: `${PREFIX_API}/api/web/logout`,
-  userInfo: `${PREFIX_API}/api/web/users/user-info`,
-  wechatAuth: `${PREFIX_API}/api/web/wechat/auth`,
-  smsRegisterCode: `${PREFIX_API}/api/web/sms/register-code`,
-  bindPhone: `${PREFIX_API}/api/web/sms/bind-phone`,
-  statisticLog: `${PREFIX_API}/api/web/statistic/log`,
+  users: "/api/web/users",
+  logout: "/api/web/logout",
+  userInfo: "/api/web/users/user-info",
+  wechatAuth: "/api/web/wechat/auth",
+  smsRegisterCode: "/api/web/sms/register-code",
+  bindPhone: "/api/web/sms/bind-phone",
+  statisticLog: "/api/web/statistic/log",
   gothrough: "/api/web/kujiale/gothrough",
   kujialeCommunity: "/api/web/kujiale/community",
   kujialeFloorplan: "/api/web/kujiale/floorplan/standard",
