4ccfbdf1694c2f14e05d17bf8cf985b5eded8325
jarisMA <jarisma@foxmail.com>
2020-12-18 18:48:54 +0800

    feat: add reply

---
+++ src/components/TheAvatar.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-avatar :size="size"
+             :src="url || userLogo
+            "></el-avatar>
+</template>
+
+<script>
+import userLogo from "images/user_logo.svg";
+export default {
+  name: 'TheAvatar',
+  props: {
+    size: {
+      type: Number,
+      default: 80
+    },
+    url: {
+      type: String,
+      default: null
+    }
+  },
+  data () {
+    return {
+      userLogo
+    }
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
--- src/views/Term/widgets/Homework.vue
+++ src/views/Term/widgets/Homework.vue
@@ -5,16 +5,14 @@
         <h4 class="homework-name">
           {{ homework.name }}
         </h4>
-        <label
-          :class="{
+        <label :class="{
             'homework-status': true,
             unsubmit: !homework.user_homework,
             uncorrected:
               homework.user_homework && homework.user_homework.status === 0,
             corrected:
               homework.user_homework && homework.user_homework.status !== 0
-          }"
-        >
+          }">
           {{
             homework.user_homework
               ? HOMEWORK_STATUS[homework.user_homework.status]
@@ -23,35 +21,29 @@
         </label>
       </div>
       <div class="homework-time-wrapper">
-        <span class="homework-end"
-          >提交截止日期：{{ formatDate(homework.end_at) }}
+        <span class="homework-end">提交截止日期：{{ formatDate(homework.end_at) }}
         </span>
       </div>
     </div>
-    <div class="fold-wrapper" v-show="fold">
+    <div class="fold-wrapper"
+         v-show="fold">
       <div class="homework-desc-wrapper">
         <div class="homework-desc">
           <label class="homework-label">
             提交要求：
           </label>
-          <div
-            class="homework-desc-content"
-            v-html="homework.description"
-          ></div>
+          <div class="homework-desc-content"
+               v-html="homework.description"></div>
         </div>
-        <el-button
-          class="homework-submit-btn"
-          type="primary"
-          :disabled="!!homework.user_homework"
-          @click="handleSubmitClick"
-        >
+        <el-button class="homework-submit-btn"
+                   type="primary"
+                   :disabled="!!homework.user_homework"
+                   @click="handleSubmitClick">
           上传作业
         </el-button>
       </div>
-      <div
-        class="homework-desc-wrapper homework-submit-wrapper"
-        v-if="homework.user_homework"
-      >
+      <div class="homework-desc-wrapper homework-submit-wrapper"
+           v-if="homework.user_homework">
         <div class="homework-desc">
           <label class="homework-label">
             小结：
@@ -62,11 +54,9 @@
         </div>
         <div class="homework-plan-card">
           <div class="card-left">
-            <the-loading-image
-              :url="homework.user_homework.study_design_pic"
-              :width="200"
-              :height="200"
-            />
+            <the-loading-image :url="homework.user_homework.study_design_pic"
+                               :width="200"
+                               :height="200" />
           </div>
           <div class="card-right">
             <h4 class="card-name">
@@ -87,15 +77,33 @@
           </div>
         </div>
       </div>
+      <div class="homework-reply-wrapper"
+           v-if="homework.user_homework.teacher_id">
+        <the-avatar :size="46"
+                    :url="homework.user_homework.teacher.avatar_url" />
+        <div class="reply-desc">
+          <label class="reply-name">
+            {{ homework.user_homework.teacher.nickname }}
+          </label>
+          <div class="reply-content-wrapper">
+            <label>作业批复：</label>
+            <p>{{ homework.user_homework.a_content }}</p>
+          </div>
+        </div>
+      </div>
     </div>
     <label class="fold-label">
-      <span v-if="!fold" @click="fold = true">
+      <span v-if="!fold"
+            @click="fold = true">
         展开
-        <icon-svg svg-class="unfold-icon" svg-name="fold" />
+        <icon-svg svg-class="unfold-icon"
+                  svg-name="fold" />
       </span>
-      <span v-else @click="fold = false">
+      <span v-else
+            @click="fold = false">
         收起
-        <icon-svg svg-class="fold-icon" svg-name="fold" />
+        <icon-svg svg-class="fold-icon"
+                  svg-name="fold" />
       </span>
     </label>
   </div>
@@ -103,6 +111,7 @@
 
 <script>
 import TheLoadingImage from "components/TheLoadingImage";
+import TheAvatar from 'components/TheAvatar';
 
 import { formatDate } from "utils/moment";
 const HOMEWORK_STATUS = {
@@ -113,7 +122,8 @@ const HOMEWORK_STATUS = {
 export default {
   name: "TermHomework",
   components: {
-    TheLoadingImage
+    TheLoadingImage,
+    TheAvatar
   },
   props: {
     homework: {
@@ -121,15 +131,15 @@ export default {
       required: true
     }
   },
-  data() {
+  data () {
     return {
       HOMEWORK_STATUS,
-      fold: true
+      fold: false
     };
   },
   methods: {
     formatDate,
-    handleSubmitClick() {
+    handleSubmitClick () {
       this.$emit("submitClick");
     }
   }
@@ -224,12 +234,60 @@ export default {
         font-size: 0px;
         line-height: 0px;
       }
-
       .homework-desc-content {
         margin-top: 4px;
         line-height: 17px;
         font-weight: 400;
         color: #ababab;
+      }
+    }
+  }
+  .homework-reply-wrapper {
+    display: flex;
+    margin-top: 20px;
+    padding-top: 15px;
+    width: 100%;
+    border-top: 1px dashed #e6e6e6ff;
+    .reply-desc {
+      flex: 1;
+      margin-left: 20px;
+      .reply-name {
+        line-height: 20px;
+        font-size: 12px;
+        font-weight: 400;
+        color: #747474;
+      }
+      .reply-content-wrapper {
+        margin-top: 6px;
+        position: relative;
+        padding: 10px;
+        background: #f5f5f5;
+        &::after {
+          position: absolute;
+          top: 7px;
+          left: -10px;
+          content: "";
+          width: 0px;
+          height: 0px;
+          border-top: 7px solid transparent;
+          border-bottom: 7px solid transparent;
+          border-right: 10px solid #f5f5f5ff;
+          font-size: 0px;
+          line-height: 0px;
+        }
+        label {
+          line-height: 20px;
+          font-size: 12px;
+          font-weight: 400;
+          color: #747474;
+        }
+        p {
+          margin-top: 4px;
+          line-height: 17px;
+          font-size: 12px;
+          font-weight: 400;
+          color: #ababab;
+        }
       }
     }
   }
@@ -308,5 +366,10 @@ export default {
   .unfold-icon {
     transform: rotate(180deg);
   }
+}
+.score-icon {
+  // position: absolute;
+  // top: 10px;
+  // right: 10px;
 }
 </style>
