25b6ffeacabb9f480a9f2228f95d17072b6822a8
jarisMA <jarisma@foxmail.com>
2021-01-21 18:13:29 +0800

    style: update style

--- src/views/Question/widgets/AnswerCard.vue
+++ src/views/Question/widgets/AnswerCard.vue
@@ -256,12 +256,12 @@ export default {
 }
 @keyframes flower2 {
   0% {
-    opacity: 0;
-    transform: scale(0.1);
-  }
-  100% {
     opacity: 1;
     transform: scale(1);
+  }
+  100% {
+    opacity: 0;
+    transform: scale(0.1);
   }
 }
 @keyframes tip {
