63374aac7bc81cb376e62d36f058391b80840332
jarisMA <jarisma@foxmail.com>
2021-03-12 15:12:59 +0800

    feat: update animate duration & sku-list add size

--- src/views/Plan/widgets/CommoditySkuList.vue
+++ src/views/Plan/widgets/CommoditySkuList.vue
@@ -11,6 +11,9 @@
         >
           <the-loading-image :width="145" :height="145" :url="sku.img_id" />
           <label class="sku-price"> ¥{{ sku.unit_price || "0.00" }} </label>
+          <div class="sku-size">
+            {{ `${sku.size_x || 0}*${sku.size_y || 0}*${sku.size_z || 0}(mm)` }}
+          </div>
         </li>
       </ul>
     </div>
@@ -61,12 +64,19 @@ export default {
     .sku-list {
       display: flex;
       flex-direction: column;
-
       .sku-item {
         position: relative;
         width: 145px;
         height: 145px;
         background: #fafafa;
+        &:hover {
+          .sku-price {
+            bottom: 26px;
+          }
+          .sku-size {
+            display: block;
+          }
+        }
         & + .sku-item {
           margin-top: 10px;
         }
@@ -80,6 +90,20 @@ export default {
           color: #666666;
           background: rgba(255, 255, 255, 0.8);
           border-radius: 8px;
+          transition: bottom 0.2s;
+        }
+        .sku-size {
+          display: none;
+          position: absolute;
+          left: 0;
+          bottom: 0;
+          padding: 0 5px;
+          width: 100%;
+          height: 21px;
+          line-height: 21px;
+          font-size: 12px;
+          color: #666666;
+          background: rgba(255, 255, 255, 0.8);
         }
       }
     }
