85476d2f541914f0b31e693c8e6ef58fdfa1e8af
Jax Chu <294242589@qq.com>
2021-04-01 11:13:36 +0800

    Revert "feat: update system tips"
    This reverts commit f6847c61ad02c4e3280c0d74a82a54d38dd1a05d.

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,22 @@
   </div>
 </template>
 <script>
-// import { isChrome } from "utils/browser-support";
+import { isChrome } from "utils/browser-support";
 import douxiGif from "images/douxi4.gif";
 export default {
   name: "App",
   created() {
-    // if (true) {
-    this.$msgBox.showMsgBox({
-      theme: "chrome_tips",
-      img: douxiGif,
-      content: "<p>系统升级中</p><p>......<p>",
-      showCancelBtn: false,
-      showConfirmBtn: false,
-      showCloseBtn: false,
-      maskCancel: false
-    });
-    // }
+    if (!isChrome()) {
+      this.$msgBox.showMsgBox({
+        theme: "chrome_tips",
+        img: douxiGif,
+        content: "<p>您的浏览器不兼容，</p><p>请下载使用 Chrome 浏览器<p>",
+        showCancelBtn: false,
+        showConfirmBtn: false,
+        showCloseBtn: false,
+        maskCancel: false
+      });
+    }
   }
 };
 </script>
